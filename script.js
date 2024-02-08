476260961e221b97967a074c8ce18215b8e916bf
marine-devweb <marine.devweb@gmail.com>
2024-02-08 12:19:46 +0100

    Demande d'accès aux données de l'API et affichage

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ const charactersContainer = document.getElementById("characters-container");
 fetch('https://rickandmortyapi.com/api/character')
 // réponse en format JSON
 .then(response => response.json())
-
-// boucle forEach pour itérer sur chaque élément du tableau characters + actions
-data.characters.forEach(character => {
-    const card = document.createElement("div");
-    card.className = "card";
-    // Affichage des données
-    card.innerHTML = `
-        <img src="${character.image}" alt="${character.name}" class="character-image">
-        <p class="character-detail"><span class="attribute">Name:</span> ${character.name}</p>
-        <p class="character-detail"><span class="attribute">Status:</span> ${character.status}</p>
-        <p class="character-detail"><span class="attribute">Species:</span> ${character.species}</p>
-    `;
-    // Utilisée pour ajouter l'élément card en tant qu'enfant de l'élément charactersContainer
-    charactersContainer.appendChild(card);
+.then(data => {
+    // boucle forEach pour itérer sur chaque élément du tableau results + actions
+    // Dans l'api le tableau est results
+    data.results.forEach(character => {
+        const card = document.createElement("div");
+        card.className = "card";
+        // Affichage des données
+        card.innerHTML = `
+            <img src="${character.image}" alt="${character.name}" class="character-image">
+            <p class="character-detail"><span class="attribute">Name:</span> ${character.name}</p>
+            <p class="character-detail"><span class="attribute">Status:</span> ${character.status}</p>
+            <p class="character-detail"><span class="attribute">Species:</span> ${character.species}</p>
+        `;
+        // Utilisée pour ajouter l'élément card en tant qu'enfant de l'élément charactersContainer
+        charactersContainer.appendChild(card);
+    });
 });
