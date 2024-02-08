5136923eb0122e99dca52d3be41dd04d6397de7d
marine-devweb <marine.devweb@gmail.com>
2024-02-08 09:37:40 +0100

    Ajout d'un objet personnage

---
+++ script.js
@@ -0,0 +1,10 @@
+const data = {
+    "characters": [
+        {
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+        }
+    ]
+}
