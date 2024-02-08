4cc2b739a46f0e09d28202f1273c944860cdff12
marine-devweb <marine.devweb@gmail.com>
2024-02-08 10:53:49 +0100

    Suppression de l'objet de données statiques

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 // API https://rickandmortyapi.com/
-const data = {
-    "characters": [
-        {
-            "name": "Rick Sanchez",
-            "status": "Alive",
-            "species": "Human",
-            "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-        },
-        {
-            "name": "Morty Smith",
-            "status": "Alive",
-            "species": "Human",
-            "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-        },
-        {
-            "name": "Summer Smith",
-            "status": "Alive",
-            "species": "Human",
-            "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-        }
-    ]
-}
+
 // récupère chaque élément du document HTML avec l'ID "characters-container"
 const charactersContainer = document.getElementById("characters-container");
 // boucle forEach pour itérer sur chaque élément du tableau characters + actions
