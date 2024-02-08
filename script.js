15ef868d0f8ae988dc737675d8e73ff8150cf053
marine-devweb <marine.devweb@gmail.com>
2024-02-08 09:42:31 +0100

    Ajout de nouveaux objets

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const data = {
     "characters": [
         {
-        "name": "Rick Sanchez",
-        "status": "Alive",
-        "species": "Human",
-        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            "name": "Rick Sanchez",
+            "status": "Alive",
+            "species": "Human",
+            "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+        }
+        {
+            "name": "Morty Smith",
+            "status": "Alive",
+            "species": "Human",
+            "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        },
+        {
+            "name": "Summer Smith",
+            "status": "Alive",
+            "species": "Human",
+            "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
         }
     ]
 }
